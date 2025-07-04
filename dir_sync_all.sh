30030e532d6382c4586fb386be73ef19897e4c3e
Marcelo Augusto <nextlevelcode014@gmail.com>
2025-07-04 10:36:25 -0300

    Fixing

--- dir_sync_all.sh
+++ dir_sync_all.sh
@@ -2,16 +2,8 @@
 
 BIN="$HOME/bin/dir_sync"
 
-$BIN "$HOME/dev/sysconfig/i3/" "$HOME/.config/i3/" &
 $BIN "$HOME/dev/sysconfig/nvim/" "$HOME/.config/nvim/"  &
-$BIN "$HOME/dev/sysconfig/fish/config.fish" "$HOME/.config/fish/config.fish" &
-$BIN "$HOME/dev/sysconfig/starship/starship.toml" "$HOME/.config/starship.toml" &
-$BIN "$HOME/dev/sysconfig/xorg.conf.d/" "/etc/X11/xorg.conf.d/"  &
-$BIN "$HOME/dev/sysconfig/optimus-manager/" "/etc/optimus-manager/" &
-$BIN "$HOME/dev/sysconfig/picom/" "$HOME/.config/picom/" &
-$BIN "$HOME/dev/sysconfig/polybar/" "$HOME/.config/polybar/" &
-$BIN "$HOME/dev/sysconfig/rofi/" "$HOME/.config/rofi/" &
-$BIN "$HOME/dev/sysconfig/alacritty/" "$HOME/.config/alacritty/" &
+$BIN "$HOME/.var/app/net.ankiweb.Anki/data/Anki2/" "$HOME/Backups/Anki2/" &
 
 wait
 
